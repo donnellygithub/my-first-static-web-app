03f6fafefce9908595900ad66e436cf9b8ef66a3
James Donnelly <james.donnelly@me.com>
2022-09-14 18:09:41 +0100

    third

---
+++ api/getImage/index.mjs
@@ -0,0 +1,15 @@
+
+module.exports = async function (context, req) {
+    let callRes = "https:\/\/images.dog.ceo\/breeds\/tervuren\/shadow_and_frisbee.jpg"
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(result => {
+        console.log(result)
+        callRes = result.message
+    })
+    .catch(err=>console.log(err))
+    context.res.json({
+        text: callRes
+    });
+    
+};
---
+++ api/message2/index.mjs
@@ -0,0 +1,5 @@
+module.exports = async function (context, req) {
+    context.res.json({
+        text: "Hello from the message2 API"
+    });
+};
