3430de90f8f4909dbdbca32875fa29ad893b1dbf
James Donnelly <james.donnelly@me.com>
2022-09-14 21:42:48 +0100

    Fixes to getImage API

--- api/getImage/index.js
+++ api/getImage/index.js
@@ -1,6 +1,7 @@
-
-module.exports = async function (context, req) {
-    let callRes = "https:\/\/images.dog.ceo\/breeds\/tervuren\/shadow_and_frisbee.jpg"
+import fetch from 'node-fetch';
+var callRes;
+export async function run(context, req) {
+    //var callRes = ""//"https:\/\/images.dog.ceo\/breeds\/tervuren\/shadow_and_frisbee.jpg"
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json())
     .then(result => {
@@ -8,8 +9,9 @@ module.exports = async function (context, req) {
         callRes = result.message
     })
     .catch(err=>console.log(err))
+    
     context.res.json({
         text: callRes
     });
-    
+    context.done;
 };
--- api/message2/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the message2 API"
-    });
-};
