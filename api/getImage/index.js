d91192f20862198dce34f4c87c652e1befa395aa
James Donnelly <james.donnelly@me.com>
2022-09-15 10:46:07 +0100

    Backtrack

--- api/getImage/index.js
+++ api/getImage/index.js
@@ -1,16 +1,16 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 var callRes;
-export async function run(context, req) {
-    //var callRes = ""//"https:\/\/images.dog.ceo\/breeds\/tervuren\/shadow_and_frisbee.jpg"
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json())
-    .then(result => {
-        console.log(result)
-        callRes = result.message
-    })
-    .catch(err=>console.log(err))
-    
+module.exports = async function (context, req) {
+    var callRes = "https:\/\/images.dog.ceo\/breeds\/tervuren\/shadow_and_frisbee.jpg"
+    // fetch("https://dog.ceo/api/breeds/image/random")
+    // .then(res => res.json())
+    // .then(result => {
+    //     console.log(result)
+    //     callRes = result.message
+    // })
+    // .catch(err=>console.log(err))
     context.res.json({
         text: callRes
-    });
+    })
+    context.done()
 };
--- api/getString/index.js
+++ api/getString/index.js
@@ -1,4 +1,4 @@
-export async function run(context, req) {
+module.exports = async function (context, req) {
     context.res.json({
         text: "Hello from the API"
     });
